a246304c35b4414ee621030e325e85735b5b5ffa
Deivid Ramon Pinto <ramon.deivid@gmail.com>
2020-05-09 17:47:41 -0300

    Implementando calculadora

---
+++ RESTFulAspNetCoreDemo - 01 - Calculator/RESTFulAspNetCoreDemo/Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RESTFulAspNetCoreDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Welcome()
+        {
+            return "Welcome to the Calculator Tabajara!.";
+        }
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double doubleValue;
+            return double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out doubleValue);
+        }
+    }
+}
